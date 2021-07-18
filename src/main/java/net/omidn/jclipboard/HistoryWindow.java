34c3dd259f0a94849dc42e1ab94cd0a5f70bcc1e
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-07-18 14:57:02 +0430

    put history window where it should be

--- src/main/java/net/omidn/jclipboard/HistoryWindow.java
+++ src/main/java/net/omidn/jclipboard/HistoryWindow.java
@@ -1,9 +1,16 @@
 package net.omidn.jclipboard;
 
 import java.awt.*;
+import java.awt.geom.PathIterator;
 
 public class HistoryWindow extends Window {
     private static HistoryWindow instance;
+
+    private int height;
+    private int width;
+    private int posY;
+    private int posX;
+
     public static HistoryWindow get(){
         if (instance==null){
             instance = new HistoryWindow();
@@ -14,18 +21,27 @@ public class HistoryWindow extends Window {
     private HistoryWindow() {
         super(null, null);
 
+        height = 500;
+        width = 500;
+        Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        posY = screenDimension.height - this.height - getTaskBarHeight();
+        posX = screenDimension.width - this.width;
+        System.out.printf("Taskbar size = %d%n", getTaskBarHeight());
 
 
     }
 
 
     public void showHistoryWindow() {
-        Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
-        System.out.println(screenDimension);
 
-        this.setBounds(500, 500, 500, 500); // will change later
+
+        this.setBounds(posX, posY, width, height); // will change later
         this.setVisible(true);
     }
 
-
+    private static int getTaskBarHeight (){
+        Dimension screeDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle maxWinSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+        return screeDimension.height - maxWinSize.height;
+    }
 }
